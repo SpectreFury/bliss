198edf5723736b9645583b9bbc93008ee705e7b9
Ayush Soni <soni.ayush.2212@gmail.com>
2025-04-24 18:02:34 +0530

    Add redirect path regardles of next

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -14,15 +14,16 @@ export async function GET(request: Request) {
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
+      const redirectPath = next || '/dashboard'
 
       console.log(origin, next);
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${origin}${redirectPath}`);
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        return NextResponse.redirect(`https://${forwardedHost}${redirectPath}`);
       } else {
-        return NextResponse.redirect(`${origin}${next}`);
+        return NextResponse.redirect(`${origin}${redirectPath}`);
       }
     }
   }
