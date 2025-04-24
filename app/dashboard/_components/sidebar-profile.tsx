75c45745171acbc9a1b9f92b621dc5e03649b544
Ayush Soni <soni.ayush.2212@gmail.com>
2025-04-24 21:00:15 +0530

    Fix error

--- app/dashboard/_components/sidebar-profile.tsx
+++ app/dashboard/_components/sidebar-profile.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { signout } from "@/app/(auth)/login/actions";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
